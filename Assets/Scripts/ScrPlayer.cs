3b9c948500b7e7d53506910a32055f89a04e7f93
Mathias Miranda <mirandaum@campusemav.com>
2021-01-18 11:02:14 +0100

    Clase 18/01/21

---
+++ Assets/Scripts/ScrPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+    
+
+    /// <summary>
+    /// ----------------------------------------------------------------------------------
+    /// DESCRIPCIÓ
+    ///         Script utilitzat per controlar el player 
+    /// AUTOR:  Mathias Miranda
+    /// DATA:   18/01/21
+    /// VERSIÓ: 0.1
+    /// CONTROL DE VERSIONS
+    ///         0.1: Moviment de la nau amb tecles i fisiques
+    /// ----------------------------------------------------------------------------------
+    /// </summary>
+    
+    
+public class ScrPlayer : MonoBehaviour
+{
+    [SerializeField] 
+    float velocitat = 10f; 
+    Vector2 movi = new Vector2();   //per calcular moviment
+    Rigidbody2D rb;                 //per accedir al component ribidbody2D
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); //Damos valor a rb
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
