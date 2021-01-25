e8f3ba42ccdf82f4371d573a449129d03387669b
Mathias Miranda <mirandaum@campusemav.com>
2021-01-25 09:35:56 +0100

    25/01/20
    Creamos las balas

--- Assets/Scripts/ScrPlayer.cs
+++ Assets/Scripts/ScrPlayer.cs
@@ -19,9 +19,23 @@ using UnityEngine;
 public class ScrPlayer : MonoBehaviour
 {
     [SerializeField] 
-    float velocitat = 10f; 
+    float velocitat = 10f;
+
+    //**********  Gestió Shot  ********
+    [SerializeField]
+    GameObject missil; // element a instancar. Arroseguem pregab!
+    [SerializeField] 
+    Transform cano; // d'on surt la bala 
+
+
+    //**********  Cooldown  *********
+    float cadencia = 0.3f; // dispara cada 5 decimes
+    float crono = 0f; // temps de cadencia
+
     Vector2 movi = new Vector2();   //per calcular moviment
     Rigidbody2D rb;                 //per accedir al component ribidbody2D
+
+    
 
     // Start is called before the first frame update
     void Start()
@@ -32,6 +46,25 @@ public class ScrPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movi.x = ETCInput.GetAxis("Horizontal") * velocitat;
+        movi.y = ETCInput.GetAxis("Vertical") * velocitat;
+
+        if (ETCInput.GetButtonDown("Shoot") && crono > cadencia) Dispara();
+        crono += Time.deltaTime;
+
+        if (ETCInput.GetButtonUp("Shoot")) crono = cadencia; // Permet disparar ràpid amb múltiples clicks
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = movi;  //Apliquem velocitat. No utilitzar Translate (son fisiques!)
+    }
+
+    void Dispara()
+    {
+        Instantiate(missil, cano.position, Quaternion.Euler(new Vector3(0, 0, 33)));
+        Instantiate(missil, cano.position, cano.rotation);
+        Instantiate(missil, cano.position, Quaternion.Euler(new Vector3(0, 0, -33)));
+        crono = 0;
     }
 }
---
+++ Assets/Scripts/ScrShot.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrShot : MonoBehaviour
+{
+    [SerializeField]
+    float vel = 20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(vel * Time.deltaTime, 0, 0);
+    }
+}
