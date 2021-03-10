733371df26e44493d3bac78124e0ace1164fd7e8
Mathias Miranda <mirandaum@campusemav.com>
2021-03-10 13:10:32 +0100

    10/03/21
    Boss més interactiu

--- Assets/Scripts/ScrBoss.cs
+++ Assets/Scripts/ScrBoss.cs
@@ -6,9 +6,38 @@ public class ScrBoss : MonoBehaviour
 {
 
     [SerializeField] GameObject explosio;
+
+    [SerializeField] Renderer r; //associar render del cos
+    Collider2D col;
+    [SerializeField] float velocitat;
+    bool moviment = true;
+
+    bool detectaPlayer = false;
+    [SerializeField] LayerMask filtreCapes;
+
+    void Start()
+    {
+        col = GetComponent<Collider2D>();
+        col.enabled = false;
+    }
+
+    void Update()
+    {
+        if (ScrControlGame.EsVisibleDesde(r, Camera.main)) col.enabled = true;
+        if (moviment) transform.Translate(velocitat * Time.deltaTime, 0, 0);
+    }
+
     void Destruccio()
     {
         Instantiate(explosio, transform.position, transform.rotation);
         Destroy(gameObject);
     }
+
+    void FixedUpdate()
+    {
+        float radiDeteccio = 20;
+        detectaPlayer = Physics2D.OverlapCircle(transform.position, radiDeteccio, filtreCapes);
+        if (detectaPlayer) moviment = false;
+    }
+
 }
